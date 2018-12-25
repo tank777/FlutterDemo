551c8ad643c4f87560e84def3ded04364250b6bb
tank777 <bhavesh.tank777@gmail.com>
2018-12-25 22:35:53 +0530

    First Commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int selectedIndex = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  void _onTaped(int index) {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: <Tab>[
+              Tab(
+                icon: Icon(Icons.directions_car),
+                text: "Car",
+              ),
+              Tab(
+                icon: Icon(Icons.directions_bike),
+                text: "Bike",
+              ),
+              Tab(
+                icon: Icon(Icons.directions_transit),
+                text: "Train",
+              )
+            ],
+          ),
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It tkes a single child and positions it
+          // in the middle of the parent.
+          child: TabBarView(
+            children:[
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_bike),
+              Icon(Icons.directions_transit)
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Home")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), title: Text("Notification")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), title: Text("Profile"))
+          ],
+          currentIndex: selectedIndex,
+          fixedColor: Colors.blue,
+          onTap: _onTaped,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
